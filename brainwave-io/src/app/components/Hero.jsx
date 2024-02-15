a84f78c820a01ecd35e3135a97d2a6bea541ae3e
hackd3n <dh@plantec.io>
2024-02-15 21:31:29 +0000

    Added Hero component using light font on the paragraph element as a similar weight to figma font.

---
+++ brainwave-io/src/app/components/Hero.jsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+    return (
+        <section className="flex flex-col w-full items-center text-center space-y-16 py-40">
+            <h2 className="text-6xl font-bold">
+                Pricing & Plans
+            </h2>
+            
+            <p className="text-lg font-light w-2/3 px-4 m-auto">
+                With lots of unique blocks, you can easily build a page without coding. Build your next consultancy website within few minutes.
+            </p>
+        </section>
+    );
+}
