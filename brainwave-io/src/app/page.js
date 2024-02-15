e8588caad16fde3917086992b07ec3d53c167401
hackd3n <dh@plantec.io>
2024-02-15 21:32:35 +0000

    added Hero section component to index page rendering.

--- brainwave-io/src/app/page.js
+++ brainwave-io/src/app/page.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <main className="">
       <div className="px-40">
-      <Header />
+        <Header />
+        <Hero />
       </div>
 
 
